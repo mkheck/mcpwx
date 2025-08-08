8eac32667440c355c1f44478143aaee22e4e9e58
Mark Heckler <mark.heckler@gmail.com>
2025-08-08 14:34:39 -0500

    STDIO

--- src/main/java/com/thehecklers/mcpwx/McpwxApplication.java
+++ src/main/java/com/thehecklers/mcpwx/McpwxApplication.java
@@ -1,26 +1,20 @@
 package com.thehecklers.mcpwx;
 
-import org.springframework.ai.tool.ToolCallback;
-import org.springframework.ai.tool.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-
 @SpringBootApplication
 public class McpwxApplication {
-    @Bean
-    List<ToolCallback> toolCallbacks(WeatherTools tools) {
-        // This will register all the tools in the WeatherTools class
-        return List.of(ToolCallbacks.from(tools));
-    }
 
-//    @Bean
-//    public ToolCallbackProvider myTools(WeatherTools weatherTools) {
-//        List<ToolCallback> tools = List.of(ToolCallbacks.from(weatherTools));
-//        return ToolCallbackProvider.from(tools);
-//    }
+    @Bean
+    public ToolCallbackProvider myTools(WeatherTools weatherTools) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(weatherTools)
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(McpwxApplication.class, args);
